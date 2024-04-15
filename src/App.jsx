51513f03a5b06a2b3f5127e71ef0cf5b069d7e9d
Iskandarov27 <150695907+Iskandarov27@users.noreply.github.com>
2024-04-15 11:55:00 +0300

    gfg
    fgfgf

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+
+
+function App() {
+  return(
+    <div className=" flex items-start">
+      <h1 className=" bg-red-800">fdsf</h1>
+      <h4>
+
+        sdsdsds
+      </h4>
+
+      <h2>
+        sdsdsd
+      </h2>
+    </div>
+  )
+
+}
+
+export default App
